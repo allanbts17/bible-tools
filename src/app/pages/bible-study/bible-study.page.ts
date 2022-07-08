45b9bdaf8dcd6ed9c2250bdf850022b19deb8dd8
allanbts17 <allanbts.18music@gmail.com>
2022-07-08 02:12:48 -0500

    bible api test

--- src/app/pages/bible-study/bible-study.page.ts
+++ src/app/pages/bible-study/bible-study.page.ts
@@ -8,12 +8,15 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class BibleStudyPage implements OnInit {
   auxLanguages
+  bibleText
   constructor(public apiService: ApiService) { }
 
   ngOnInit() {
-    this.apiService.getRVBibleBookList().subscribe(bibles => {
+    /*this.apiService.getAllBibles().subscribe(bibles => {
+     // var obj = bibles
+      ///this.bibleText = obj
       console.log(bibles)
-    },(err)=> console.log(err))
+    },(err)=> console.log(err))*/
    /*this.apiService.getAllBibles().subscribe((bibles)=>{
       console.log(bibles)
     },(error)=>{
@@ -32,6 +35,26 @@ export class BibleStudyPage implements OnInit {
     /*this.apiService.getBiblesByLanguageId("spa").then((bibles)=>{
       console.log(bibles)
     })*/
+    this.apiService.getBibleBookList('b32b9d1b64b4ef29-01').subscribe((books)=>{
+      console.log(books)
+      this.apiService.getChapterList('b32b9d1b64b4ef29-01','GEN').subscribe((bibles)=>{
+        console.log(bibles)
+      })
+      this.apiService.getChapter('b32b9d1b64b4ef29-01','GEN.1').subscribe((bibles)=>{
+        console.log(bibles)
+        this.bibleText = bibles
+        setTimeout(() => {
+          var len = document.getElementsByTagName("span").length
+          for(let i=0;i<len;i++){
+            document.getElementsByTagName("span")[i].style.fontWeight = "500"
+            document.getElementsByTagName("span")[i].style.marginRight = "5px"
+          }
+
+        })
+      })
+    })
+
+    //'b32b9d1b64b4ef29-01'
 
   }
 
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,12 +11,14 @@ export class ApiService {
   auxBibles
   base_api_endpoint = "https://api.scripture.api.bible"
   spanish_api_endpoint = "https://bible-app-demo.herokuapp.com/api"
+  bible_com_key = 'da2da2615f04e97a3bac7d9aba71615d'
+  bible_com_endpoint = 'https://api.biblia.com/v1/bible'
   reqHeader = new HttpHeaders({
     'Content-Type': 'application/json',
     'api-key':'30a8f65d4c9d905424846450db90b830'
   })
   RV_reqHeader = new HttpHeaders({
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/html',
     'Access-Control-Allow-Origin':'*'
   })
 
@@ -25,6 +27,36 @@ export class ApiService {
   getAllBibles(){
     const path = `${this.base_api_endpoint}`+"/v1/bibles";
     return this.http.get(path,{headers: this.reqHeader});
+  }
+ /* getAllBibles(){
+    const path = `${this.bible_com_endpoint}`+"/find?key="+this.bible_com_key;
+    return this.http.get(path);
+  }*/
+
+  getBibleBookList(id){
+    const path = `${this.base_api_endpoint}`+"/v1/bibles/"+`${id}`+"/books";
+    return this.http.get(path,{headers: this.reqHeader});
+  }
+
+  getChapterList(bibleId,bookId){
+    const path = `${this.base_api_endpoint}`+"/v1/bibles/"+`${bibleId}`+"/books/"+`${bookId}`+"/chapters";
+    return this.http.get(path,{headers: this.reqHeader});
+  }
+
+  getChapter(bibleId,chapterId){
+    const path = `${this.base_api_endpoint}`+"/v1/bibles/"+`${bibleId}`+"/chapters/"+`${chapterId}`;
+    return this.http.get(path,{headers: this.reqHeader});
+  }
+
+  getBookSectionList(bibleId,bookId){
+    const path = `${this.base_api_endpoint}`+"/v1/bibles/"+`${bibleId}`+"/books/"+`${bookId}`+"/sections";
+    return this.http.get(path,{headers: this.reqHeader});
+  }
+
+  getBibleContent(){
+
+    const path = `${this.bible_com_endpoint}`+"/content/rvr60.html.js?passage=Jn&key="+this.bible_com_key;
+    return this.http.get(path,{headers:this.RV_reqHeader});
   }
 
   getBible(id){
