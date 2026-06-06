5badc2237c7f47fb516367ba97963fbb4b781f77
Allan Bennett <allanbts.18music@gmail.com>
2026-06-06 15:15:49 -0500

    ng update @angular/core@21 @angular/cli@21

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { defineCustomElements as jeepSqlite} from 'jeep-sqlite/loader';
+import { defineCustomElements as jeepSqlite } from 'jeep-sqlite/loader';
 import { Capacitor } from '@capacitor/core';
 import { CapacitorSQLite, SQLiteConnection } from '@capacitor-community/sqlite';
 
@@ -14,11 +14,11 @@ if (environment.production) {
 jeepSqlite(window);
 window.addEventListener('DOMContentLoaded', async () => {
   const platform = Capacitor.getPlatform();
-  const sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite)
+  const sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite);
   try {
-    if(platform === "web") {
+    if (platform === 'web') {
       //console.log('in index.ts')
-      const jeepEl = document.createElement("jeep-sqlite");
+      const jeepEl = document.createElement('jeep-sqlite');
       document.body.appendChild(jeepEl);
       await customElements.whenDefined('jeep-sqlite');
       //console.log('in index.ts after customElements')
@@ -27,11 +27,13 @@ window.addEventListener('DOMContentLoaded', async () => {
     }
     await sqlite.checkConnectionsConsistency();
 
-    platformBrowserDynamic().bootstrapModule(AppModule)
-      .catch(err => console.log(err));
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule, {
+        applicationProviders: [provideZoneChangeDetection()],
+      })
+      .catch((err) => console.log(err));
   } catch (err) {
     console.log(`Error: ${err}`);
-    throw new Error(`Error: ${err}`)
+    throw new Error(`Error: ${err}`);
   }
-
 });
